520adf78c9600ff855c1ee3c41b76e3486d6470b
PutuRivan <150578221+PutuRivan@users.noreply.github.com>
2025-01-12 21:07:15 +0700

    fix:toast hooks

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -18,12 +18,12 @@ type ToasterToast = ToastProps & {
   action?: ToastActionElement
 }
 
-const actionTypes = {
-  ADD_TOAST: "ADD_TOAST",
-  UPDATE_TOAST: "UPDATE_TOAST",
-  DISMISS_TOAST: "DISMISS_TOAST",
-  REMOVE_TOAST: "REMOVE_TOAST",
-} as const
+// const actionTypes :{
+//   readonly ADD_TOAST: "ADD_TOAST",
+//   readonly UPDATE_TOAST: "UPDATE_TOAST",
+//   readonly DISMISS_TOAST: "DISMISS_TOAST",
+//   readonly REMOVE_TOAST: "REMOVE_TOAST",
+// }
 
 let count = 0
 
@@ -32,7 +32,12 @@ function genId() {
   return count.toString()
 }
 
-type ActionType = typeof actionTypes
+type ActionType = {
+  ADD_TOAST: "ADD_TOAST";
+  UPDATE_TOAST: "UPDATE_TOAST";
+  DISMISS_TOAST: "DISMISS_TOAST";
+  REMOVE_TOAST: "REMOVE_TOAST";
+}
 
 type Action =
   | {
